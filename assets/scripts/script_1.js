a75c40275f39ed60448ad9143003e8c08e191ea1
p13mout@ionio.gr <p13mout@ionio.gr>
2019-01-16 00:23:23 +0200

    restored width

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -1,6 +1,7 @@
 // Script 1
 // Data Visualization III - Pie Chart
 var donut = donutChart()
+        .width(960)
         .height(450)
         .cornerRadius(0) // sets how rounded the corners are on each slice
         .padAngle(0.005) // effectively dictates the gap between slices
